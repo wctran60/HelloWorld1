3640c74b948979b185cf947ee1b0cbf9e180291f
wctran60 <wctran@yahoo.com>
2022-10-18 23:42:09 -0400

    Update HelloWorld.java

--- William/src/main/java/com/example/william/HelloWorld.java
+++ William/src/main/java/com/example/william/HelloWorld.java
@@ -2,8 +2,8 @@ package com.example.william;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world!  This is main class");
-        System.out.println("Hello world!  This is test branch 2");
+        System.out.println("HelloWorld!  This is main class.");
+        System.out.println("Below are additional test branches from teammates.");
         William myWilliam = new William();
         myWilliam.message();
 
